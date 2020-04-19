832c65271bd1e1e9fdee0b8ac41001cc734dbfc6
Andreas Schiffler <schiffler@roschi.eu>
2020-04-19 15:58:05 +0000

    added nicer front end for the training and classification

--- ml5_train_classification/index.js
+++ ml5_train_classification/index.js
@@ -6,12 +6,29 @@ let imagesOfA = 0;
 let imagesOfB = 0;
 let classificationResult;
 let confidence = 0; 
+let mycanvas;
+let running_state = false;
+let trained_state = false;
+let shutterSound = 0;
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
 
 function setup() {
-  createCanvas(400, 400);
+  shutterSound = loadSound('shutter.mp3');
+//  mycanvas = createCanvas(300, 300);
+  mycanvas=createCanvas(windowWidth, 300);
+  mycanvas.parent("canvasContainer");
   // Create a video element
-  video = createCapture(VIDEO);
+  if (isMobileDevice()){
+    video = createCapture({audio: false,video: {facingMode: {exact: "environment"}}});
+  }else{
+    video = createCapture(VIDEO);
+  }
   video.size(300, 300);
+  video.position(windowWidth*0.05,0);
+  rectMode(CENTER);
   //video.hide();
   // Load the features from existing Model
   featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
@@ -22,6 +39,13 @@ function setup() {
   setupButtons();
   noLoop();
   uibuilder.start()
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, 400);
+  video.position(windowWidth*0.05,0);
+
 }
 
 // A function to be called when the model has been loaded
@@ -41,7 +65,8 @@ function setupButtons() {
   // from the video with a label of "A" to the classifier
   buttonA = select('#ButtonA');
   buttonA.mousePressed(function() {
-    classifier.addImage('A');
+    shutterSound.play();
+    classifier.addImage('Class 2');
     select('#amountOfAImages').html(++imagesOfA);
   });
   
@@ -49,26 +74,47 @@ function setupButtons() {
   // from the video with a label of "B" to the classifier
   buttonB = select('#ButtonB');
   buttonB.mousePressed(function() {
-    classifier.addImage('B');
+    shutterSound.play();
+    classifier.addImage('Class 1');
     select('#amountOfBImages').html(++imagesOfB);
   });
 
   // Train Button
   train = select('#train');
   train.mousePressed(function() {
+    running_state = false;
+    buttonPredict.html("Start Classification");
+    if (imagesOfA > 5  || imagesOfB >5){
     classifier.train(function(lossValue) {
+      train.html("Training started, please wait.");
       if (lossValue) {
         loss = lossValue;
-        select('#loss').html('Loss: ' + loss);
+        select('#loss').html('Train on ' + (imagesOfA+imagesOfB) + ' Images, Loss: ' + loss);
+        trained_state = false;
       } else {
-        select('#loss').html('Done Training! Final Loss: ' + loss);
+        select('#loss').html('Done Training on ' + (imagesOfA+imagesOfB) + ' Final Loss: ' + loss);
+        trained_state = true;
+        train.html("Trained Model available, Press to retrain");
+        //running_state = true;
+        //classify();
       }
     });
+    } else {
+      window.alert("too few images cpature. Capture at least 5 images for each class");
+    }
   });
 
   // Predict Button
   buttonPredict = select('#buttonPredict');
-  buttonPredict.mousePressed(classify);
+  buttonPredict.mousePressed(function(){
+    if (trained_state){
+      running_state = true;
+      buttonPredict.html("Classification runs");
+      classify();
+    } else {
+      window.alert("Train the model first");
+    }      
+  });
   
 }
 
@@ -90,13 +136,14 @@ function gotResults(err, result) {
   }
   if(select('#sendtonodered').checked()){
     //console.log("sendtonodered");
-    uibuilder.send( { 'topic': 'from-the-front', 'payload': result[0] } )
+    uibuilder.send( { 'topic': 'send-from-web-client', 'payload': result[0] } )
   }
   select('#result').html(result[0].label);
-  select('#confidence').html(result[0].confidence);
+  select('#confidence').html(nf(result[0].confidence,0,2));
 
   classificationResult = result[0].label;
   confidence = result[0].confidence;
-
-  classify();
+  if (running_state){
+    classify();
+  }
 }
