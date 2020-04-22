41c0f1b45f30fe95a64c94eab25dc2bffb292097
Andreas Schiffler <schiffler@roschi.eu>
2020-04-22 08:38:15 +0000

    corrected typo in the environment varaible

--- docker-nodered/README.md
+++ docker-nodered/README.md
@@ -1,1 +1,9 @@
-sudo docker build -t nodered-ml5 -f docker-nodered/Dockerfile .
+Build the docker image
+docker build -t nodered-ml5 -f docker-nodered/Dockerfile .
+
+Run the docker container with static self-signed certififcate
+docker run -it -p 1880:1880 nodered-ml5
+
+Run the docker container with automatic created certificate
+docker run -it -p 1880:1880 -p 80:80 -e EMAIL=mail@example.com -e LETSENCRYPTDOMAIN=example.com --entrypoint /bin/bash nodered-ml5 -c /usr/src/node-red/start.sh
+
--- docker-nodered/start.sh
+++ docker-nodered/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL --domain $LETSCRYPTDOMIAN
-cp /etc/letsencrypt/live/$LETSCRYPTDOMIAN/cert.pem /data/cert.pem
-cp /etc/letsencrypt/live/$LETSCRYPTDOMIAN/privkey.pem /data/key.pem
+certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL --domain $LETSENCRYPTDOMAIN 
+cp /etc/letsencrypt/live/$LETSENCRYPTDOMAIN/cert.pem /data/cert.pem
+cp /etc/letsencrypt/live/$LETSENCRYPTDOMAIN/privkey.pem /data/key.pem
 npm start -- --userDir /data
