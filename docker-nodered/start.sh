bcafe10bdd902f95f8c21c549c573de1092da247
Andreas Schiffler <schiffler@roschi.eu>
2020-04-22 08:21:06 +0000

    removed --stating option in certbot now productive

--- docker-nodered/start.sh
+++ docker-nodered/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-certbot certonly --standalone --non-interactive --agree-tos --staging --email $EMAIL --domain $LETSCRYPTDOMIAN
+certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL --domain $LETSCRYPTDOMIAN
 cp /etc/letsencrypt/live/$LETSCRYPTDOMIAN/cert.pem /data/cert.pem
 cp /etc/letsencrypt/live/$LETSCRYPTDOMIAN/privkey.pem /data/key.pem
 npm start -- --userDir /data
