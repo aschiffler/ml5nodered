02247e7633b27adf54e0f0344ce1355543241f69
Andreas Schiffler <schiffler@roschi.eu>
2020-04-22 07:23:40 +0000

    dded start script for letsencrypt certbot

---
+++ docker-nodered/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+certbot certonly --standalone --non-interactive --agree-tos --staging --email $EMAIL --domain $LETSCRYPTDOMIAN
+cp /etc/letsencrypt/live/$LETSCRYPTDOMIAN/cert.pem /data/cert.pem
+cp /etc/letsencrypt/live/$LETSCRYPTDOMIAN/privkey.pem /data/key.pem
+npm start
