691faba7a2dd4c0e455c5f92177b50530f6f15de
Andreas Schiffler <schiffler@roschi.eu>
2020-05-01 17:22:50 +0000

    added individual secret included in flow infowq

--- docker-nodered/start.sh
+++ docker-nodered/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+flow=$(<flows.json)
+echo ${flow//myhashedsecret/$SECRET} > flows.json
 certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL --domain $LETSENCRYPTDOMAIN 
 cp /etc/letsencrypt/live/$LETSENCRYPTDOMAIN/cert.pem /data/cert.pem
 cp /etc/letsencrypt/live/$LETSENCRYPTDOMAIN/privkey.pem /data/key.pem
