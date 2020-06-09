8974638f89d5bc51db5a0ab51cae01060dcb978e
Andreas Schiffler <schiffler@roschi.eu>
2020-06-09 08:38:58 +0000

    change name for env PORT to NODEREDPORT

--- docker-nodered/settings.js
+++ docker-nodered/settings.js
@@ -20,7 +20,7 @@ var fs = require("fs");
 
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
-    uiPort: process.env.PORT || 1880,
+    uiPort: process.env.NODEREDPORT || 1880,
 
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // To listen on all IPv6 addresses, set uiHost to "::",
